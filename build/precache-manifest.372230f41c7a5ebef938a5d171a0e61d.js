a49e800a2af046c585742915143afb81738b95a6
Phil Forster <46075996+Melicity76@users.noreply.github.com>
2020-09-08 15:13:50 +0100

    Add files via upload

---
+++ build/precache-manifest.372230f41c7a5ebef938a5d171a0e61d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5066ea8f2f49dd114473175d99ecb850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf87fd27d0b752736ef",
+    "url": "/static/css/main.eade637f.chunk.css"
+  },
+  {
+    "revision": "e48935c9837da17c16d7",
+    "url": "/static/js/2.9d2230eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d2230eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf87fd27d0b752736ef",
+    "url": "/static/js/main.f96b5e85.chunk.js"
+  },
+  {
+    "revision": "2ebfaff571e5f67d4e25",
+    "url": "/static/js/runtime-main.009aeb4a.js"
+  },
+  {
+    "revision": "e099ecabd590840041dbbcaf1c45773e",
+    "url": "/static/media/bg1.e099ecab.jpg"
+  },
+  {
+    "revision": "35ad3697636a4ff30fa6f5d0ddcbf1ff",
+    "url": "/static/media/nfc.35ad3697.png"
+  },
+  {
+    "revision": "09f83debf7e133522f9e2ca3a8716fc0",
+    "url": "/static/media/nfchova.09f83deb.png"
+  },
+  {
+    "revision": "cd7bb6bec60f6799aa174b4e07b92ea4",
+    "url": "/static/media/reactlogo.cd7bb6be.png"
+  },
+  {
+    "revision": "bd23426ed9c0a2489e4b22302474fa71",
+    "url": "/static/media/titlemainblue.bd23426e.png"
+  }
+]);
